c96b257fa23e2875b13903a3b9f6e81500d9ae71
dipakmp7 <dmpatil455@gmail.com>
2023-11-24 15:12:49 +0530

    done with the login form validation

---
+++ frontend/src/LoginValidation.js
@@ -0,0 +1,30 @@
+function Validation(values){
+    // alert("")
+    let error = {}
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-z0-9]{8,}$/
+
+    if(values.email === ""){
+        error.email = "Email should not be empty"
+    }
+    else if(!emailPattern.test(values.email)){
+        error.email = "Email did not match"
+    }
+    else{
+        error.email = ""
+    }
+
+    if(values.password === ""){
+        error.password = "Password should not be empty"
+    }
+    else if(!passwordPattern.test(values.password)){
+        error.password = "Password did not match"
+    }
+    else{
+        error.password = ""
+    }
+
+    return error
+}
+
+export default Validation
