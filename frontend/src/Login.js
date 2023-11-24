f40594c553de8d2963ef32d9f83e84ef23b25fa1
dipakmp7 <dmpatil455@gmail.com>
2023-11-24 12:23:57 +0530

    frontend dependencies added (react)

---
+++ frontend/src/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Login (){
+    return (
+        <div>
+            <div>
+                <form action="">
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" placeholder="Enter Your Email"></input>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login
