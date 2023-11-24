25b38c61dbea42f3b0a34479e1d0825fdae6c7ab
dipakmp7 <dmpatil455@gmail.com>
2023-11-24 13:10:43 +0530

    add bootstrap in login file

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -2,20 +2,20 @@ import React from "react";
 
 function Login (){
     return (
-        <div>
-            <div>
+        <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
+            <div className="bg-white p-3 rounded w-25">
                 <form action="">
                     <div className="mb-3">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" placeholder="Enter Your Email"></input>
+                        <label htmlFor="email"><strong>Email</strong></label>
+                        <input type="email" placeholder="Enter Your Email" className="form-control rounded-0"></input>
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" placeholder="Enter Your Password"></input>
+                        <label htmlFor="password"><strong>Password</strong></label>
+                        <input type="password" placeholder="Enter Your Password" className="form-control rounded-0"></input>
                     </div>
-                    <button className="btn btn-success">Login</button>
+                    <button className="btn btn-success w-100">Login</button>
                     <p>You are agree to our term and policies</p>
-                    <button className="btn btn-default border">Create Account</button>
+                    <button className="btn btn-default border w-100 bg-light">Create Account</button>
                 </form>
             </div>
         </div>
